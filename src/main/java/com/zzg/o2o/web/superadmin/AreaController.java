b4cfcfd694bd0a63d9414898a52985b68f032a64
zhaozigang <1198952476@qq.com>
2018-06-29 23:08:32 +0800

    添加日志模块配置

--- src/main/java/com/zzg/o2o/web/superadmin/AreaController.java
+++ src/main/java/com/zzg/o2o/web/superadmin/AreaController.java
@@ -2,6 +2,8 @@ package com.zzg.o2o.web.superadmin;
 
 import com.zzg.o2o.entity.Area;
 import com.zzg.o2o.service.AreaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,20 +18,21 @@ import java.util.Map;
 @Controller
 @RequestMapping("/superadmin")
 public class AreaController {
+    Logger logger = LoggerFactory.getLogger(AreaController.class);
     @Autowired
     private AreaService areaService;
     @RequestMapping(value = "/listarea", method = RequestMethod.GET)
     @ResponseBody  //装换为json
     private Map<String, Object> listArea(){
+        logger.info("===start===");
+        long startTime = System.currentTimeMillis();
         Map<String, Object> modelMap= new HashMap<String, Object>();
         List<Area> list = new ArrayList<Area>();
-        System.out.println("==========================");
         try {
             list = areaService.getAreaList();
 
             for (Area area : list){
                 System.out.println(area.getAreaName());
-                System.out.println("<><><><><><><>");
             }
             modelMap.put("rows", list);
             modelMap.put("total", list.size());
@@ -38,6 +41,8 @@ public class AreaController {
             modelMap.put("success", false);
             modelMap.put("errMsg", e.toString());
         }
+        logger.error("test error");
+        logger.debug("total time: [{}ms]", System.currentTimeMillis() - startTime);
         return modelMap;
     }
 
